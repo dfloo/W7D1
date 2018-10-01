8bfbfc0feca038e7f8c251e61ef222a82fd934f0
Devin <devin.p.flood@gmail.com>
2018-10-01 14:28:43 -0700

    Add rootReducer, todosReducer, and todoActions

---
+++ todos_part1/frontend/reducers/todos_reducer.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+import { RECEIVE_TODOS } from '../actions/todo_actions';
+
+const todosReducer = (state = {}, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
